e305a2395328e03a42d247660a50535a83d8ce1a
drorpip <dror.pipano@gmail.com>
2021-01-17 23:34:30 +0200

    fix pkl

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('model.pkl','rb'))
+# model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -21,7 +22,6 @@ def predict():
     output = round(prediction[0], 2)
 
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
-
 
 if __name__ == "__main__":
     app.run(debug=True)
